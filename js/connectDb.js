62e1984ae67e5e8dc5cbfc20f9638ce868ed5ade
ferrinip <126610081+ferrinip@users.noreply.github.com>
2024-02-02 14:53:54 -0400

    Delete js directory

--- js/connectDb.js
+++
@@ -1,4 +0,0 @@
-const data = ["dbEnglish","dbManager","dbEngineer","dbQuality","dbComputing","dbProgrammer","dbMathematics","dbMedicine","dbGlossary"];
-data.forEach(function (element, index) {
-  document.write(`<script src="db/${element}.db"></script>`);
-});
